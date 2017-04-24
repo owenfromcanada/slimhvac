122b8d582b7c9e4620a90d540f902ac21d3bf419
Owen Tosh <owen.tosh@Nexteer.Com>
2017-04-24 13:35:24 -0400

    added proper test case, checking for page title

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class BasicViewTest(unittest.TestCase):  
 
-assert 'Django' in browser.title
+    def setUp(self):  
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):  
+        self.browser.quit()
+
+    def test_check_current_temperature(self):
+        # Adam wants to check the current temperature reading of the thermostat.
+        # He navigates to the home page.
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header shows the product name
+        self.assertIn('SlimHVAC', self.browser.title)
+        self.fail('Finish the test!')
+        
+        # He can see the current temperature displayed
+
+if __name__ == '__main__':
+    unittest.main()
