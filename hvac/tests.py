1d31c363c1d0925132528e25d1f017552882a8c2
Owen Tosh <owen.tosh@Nexteer.Com>
2017-04-26 13:13:04 -0400

    added basic home page

--- hvac/tests.py
+++ hvac/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from hvac.views import start_page  
 
 class MainPageTest(TestCase):
@@ -7,3 +9,11 @@ class MainPageTest(TestCase):
     def test_root_url_resolves_to_start_page_view(self):
         found = resolve('/')  
         self.assertEqual(found.func, start_page)  
+
+    def test_start_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = start_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>SlimHVAC</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- hvac/views.py
+++ hvac/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def start_page():
-    pass
+def start_page(request):
+    return render(request, 'start.html')
