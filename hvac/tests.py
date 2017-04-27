6ef4cd435b2d90c465902622dddb8210bdf36cbc
Owen Tosh <owen.tosh@Nexteer.Com>
2017-04-27 08:41:25 -0400

    added Thermostat model

--- hvac/tests.py
+++ hvac/tests.py
@@ -1,7 +1,8 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 
-from hvac.views import start_page  
+from hvac.models import Thermostat
+from hvac.views import start_page
 
 class StartPageTest(TestCase):
 
@@ -12,6 +13,17 @@ class StartPageTest(TestCase):
     def test_start_page_template_used(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'start.html')
+    
+    def test_start_page_redirects_after_post_request(self):
+        response = self.client.post('/', data={'name_text': 'Thermostat name', 'zwave_id_text': '1'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+    
+    def test_new_thermostat_saved(self):
+        self.assertEqual(Thermostat.objects.count(), 0)
+        self.client.post('/', data={'name_text': 'Thermostat name', 'zwave_id_text': '1'})
+        self.assertEqual(Thermostat.objects.count(), 1)
+        self.assertEqual(Thermostat.objects.first().name, 'Thermostat name')
 
 
 class ThermostatPageTest(TestCase):
@@ -20,3 +32,22 @@ class ThermostatPageTest(TestCase):
         response = self.client.get('/thermostat/new')
         self.assertTemplateUsed(response, 'new.html')
 
+
+class ThermostatModelTest(TestCase):
+    
+    def test_saving_and_retrieving_thermostats(self):
+        first_therm = Thermostat()
+        first_therm.name = 'Name of first'
+        first_therm.zwave_id = 1
+        first_therm.save()
+        
+        second_therm = Thermostat()
+        second_therm.name = 'Second thermostat'
+        second_therm.zwave_id = 2
+        second_therm.save()
+        
+        saved_therms = Thermostat.objects.all()
+        self.assertEqual(saved_therms.count(), 2)
+        self.assertEqual(saved_therms[0].name, 'Name of first')
+        self.assertEqual(saved_therms[1].name, 'Second thermostat')
+        
--- hvac/views.py
+++ hvac/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from hvac.models import Thermostat
 
 def start_page(request):
+    if request.method == 'POST':
+        Thermostat.objects.create(name=request.POST['name_text'], zwave_id=request.POST['zwave_id_text'])
+        return redirect('/')
     return render(request, 'start.html')
 
 def new_thermostat(request):
